2ef4793f0b792a36f9b7cb90e8064e2ac2e40844
Gabriel Ayres <gabriel.ar.fort@gmail.com>
2023-07-20 11:21:09 -0300

    Fix(main section): Adição da classe active ao botão clicado.

--- src/js/modules/changeText.js
+++ src/js/modules/changeText.js
@@ -6,10 +6,12 @@ export default function changeTextOnDisplay() {
   buttons.forEach((btn) => {
     function handleClick() {
       Dom.removeClass("visible");
+      Dom.removeClass("active");
 
       const reference = this.getAttribute("data-refers-to");
 
       Dom.setVisible(`[data-${reference}]`);
+      this.classList.add("active");
     }
 
     btn.addEventListener("click", handleClick);
